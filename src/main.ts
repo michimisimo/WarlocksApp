5a5894575dd7fb812949db130f822f48bf2ae830
Felipe Urbina <furbina@demafront.cl>
2025-05-02 12:01:20 -0400

    se cambia la informacion de acceso en home y se integran cambios esteticos

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { provideRouter, RouteReuseStrategy, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { IonicStorageModule, Storage } from '@ionic/storage-angular';
+
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
-import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
+import { importProvidersFrom, LOCALE_ID } from '@angular/core';
 import localeEs from '@angular/common/locales/es';
 
 registerLocaleData(localeEs);
@@ -18,5 +20,7 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(),
     { provide: LOCALE_ID, useValue: 'es-ES' },
+    importProvidersFrom(IonicStorageModule.forRoot()),
+    Storage
   ],
 });
