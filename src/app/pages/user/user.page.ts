1a9277dc5c658c168e22c965157695aa2a02d051
ferespina <106361186+ferespina@users.noreply.github.com>
2025-05-04 11:10:43 -0400

    se genera la logica para cerrar sesion y eliminar el currentUser del storage

--- src/app/pages/user/user.page.ts
+++ src/app/pages/user/user.page.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { BannerTopComponent } from 'src/app/components/banner-top/banner-top.component';
-
+import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.page.html',
   styleUrls: ['./user.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, BannerTopComponent]
+  imports: [CommonModule, FormsModule, BannerTopComponent, IonicModule]
 })
 export class UserPage implements OnInit, AfterViewInit {
 
@@ -21,19 +21,20 @@ export class UserPage implements OnInit, AfterViewInit {
   activeTab = 'Mi Cuenta';
 
   constructor(
-    private router: Router
+    private router: Router,
+    private userService: UserService,
   ) { }
 
   ngOnInit() {
     this.activeTab = 'Mi cuenta';
-    this.updateCategoryVisibility();
+    this.showCategoria();
   }
 
   ngAfterViewInit() {
-    this.updateCategoryVisibility();
+    this.showCategoria();
   }
 
-  updateCategoryVisibility() {
+  showCategoria() {
     // Si estamos en 'Mi cuenta', ocultamos las categorías
     if (this.componente) {
       this.componente.showCat3 = this.activeTab === 'Usuarios';
@@ -42,11 +43,16 @@ export class UserPage implements OnInit, AfterViewInit {
 
   cambiarTab(tab: string) {
     this.activeTab = tab;
-    this.updateCategoryVisibility();
+    this.showCategoria();
   }
 
   volverAInicio(){
     this.router.navigate(['/inicio'])
+  }
+
+  cerrarSesion(){
+    this.userService.removeCurrentUser();
+    this.router.navigate(['/home'])
   }
 
 }
