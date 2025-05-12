b82405902d6eb80146320e36e9fc8330cc656e86
ferespina <106361186+ferespina@users.noreply.github.com>
2025-05-12 18:52:53 -0400

    se agrega logica de validacion para el componente cardcampass, en donde en el campo de contraseña nueva debe coincidir con el campo de validacion de contraseña

--- src/app/components/card-campass/card-campass.component.ts
+++ src/app/components/card-campass/card-campass.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-card-campass',
@@ -14,9 +16,40 @@ export class CardCampassComponent  implements OnInit {
 
   nuevaContrasena: string = '';
   confirmarContrasena: string = '';
+  contrasenaActual: string='';
 
-  constructor() { }
+  constructor(
+    private alertCtrl: AlertController,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {}
+
+  async mostrarAlertaExito() {
+  const alerta = await this.alertCtrl.create({
+    header: 'Éxito',
+    message: 'La contraseña se cambió exitosamente.',
+    buttons: ['OK'],
+  });
+  await alerta.present();
+}
+
+  async cambiarContrasena(){
+    if (this.nuevaContrasena !== this.confirmarContrasena){
+      const alert = await this.alertCtrl.create({
+        header: 'Error',
+        message: 'las contraseñas no coinciden.',
+        buttons: ['Ok']
+      });
+      await alert.present();
+      return;
+    }
+    this.mostrarAlertaExito();
+    this.contrasenaActual = '';
+    this.nuevaContrasena = '';
+    this.confirmarContrasena = '';
+  }
+
+  
 
 }
